41358a7c0918d9c5557086a6e648813ad3ea7f4c
xebec19 <rohant946@gmail.com>
2022-04-17 16:42:05 +0530

    save

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,54 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = { counter: 0, showCounter: true }
 
-const counterReducer = (state = { counter: 0 }, action) => {
+createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state,action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
+
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment')
     {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
     if(action.type === 'decrement')
     {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
         }
     }
     if(action.type === 'incrementBy5')
     {
         return {
-            counter: state.counter + action.amount
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        }
+    }
+
+    if(action.type === 'toggle')
+    {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
 
